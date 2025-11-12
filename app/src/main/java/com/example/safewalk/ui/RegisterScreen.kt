e1f3f5f5ba59f664fedbc494814fb21c47d26dae
Diego Martinez Auqui <90728005+FazeDiego@users.noreply.github.com>
2025-11-12 11:28:17 -0300

    Scroll en RegisterScreen para dispositivos pequeños
    -

--- app/src/main/java/com/example/safewalk/ui/RegisterScreen.kt
+++ app/src/main/java/com/example/safewalk/ui/RegisterScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -79,7 +81,8 @@ fun RegisterScreen(navController: NavController) {
                 Column(
                     modifier = Modifier
                         .align(Alignment.TopCenter)
-                        .padding( top = 20.dp),
+                        .verticalScroll(rememberScrollState())
+                        .padding(top = 20.dp),
 
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
